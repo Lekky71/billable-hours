76145c57dafb1ca4cf9676c7becc4614d89f8eae
Oluwaleke Fakorede <hashilekky@gmail.com>
2019-07-24 14:07:47 +0100

    completed mocha test

--- app/tests/bill.test.js
+++ app/tests/bill.test.js
@@ -2,13 +2,13 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const chaiAsPromised = require('chai-as-promised');
 const app = require('../index');
-
+const fs = require('fs');
 const expect = chai.expect;
 
 chai.use(chaiHttp);
 chai.use(chaiAsPromised);
-const sampleFilePath = __dirname.replace('app/lib', 'sample/sample_projects_file.csv');
-const invalidFilePath = __dirname.replace('app/lib', 'sample/backblue.gif');
+const sampleFilePath = __dirname.replace('app/tests', 'sample/sample_projects_file.csv');
+const invalidFilePath = __dirname.replace('app/tests', 'sample/backblue.gif');
 
 describe('Billable Hours Suite', () => {
 
@@ -22,7 +22,7 @@ describe('Billable Hours Suite', () => {
         }
         expect(res.body.code)
           .to
-          .have
+          .be
           .equal(400);
         expect(res.body)
           .to
@@ -30,7 +30,7 @@ describe('Billable Hours Suite', () => {
           .property('message');
         expect(res.body.message)
           .to
-          .have
+          .be
           .equal('timetable file is required');
         done();
       });
@@ -41,14 +41,14 @@ describe('Billable Hours Suite', () => {
     chai.request(app)
       .post('/upload')
       .type('form')
-      .attach('timetable_file', fs.readFileSync(invalidFilePath))
+      .attach('timetable_file', fs.readFileSync(invalidFilePath), 'backblue.gif')
       .end((err, res) => {
         if (err) {
           console.log(err);
         }
         expect(res.body.code)
           .to
-          .have
+          .be
           .equal(400);
         expect(res.body)
           .to
@@ -56,33 +56,27 @@ describe('Billable Hours Suite', () => {
           .property('message');
         expect(res.body.message)
           .to
-          .have
+          .be
           .equal('file is not a csv file');
         done();
       });
 
   });
 
-  it('should return error because file is not sent', (done) => {
+  it('should return zip file with status 200', (done) => {
     chai.request(app)
       .post('/upload')
       .type('form')
-      .attach('imageField', fs.readFileSync('avatar.png'), 'avatar.png')
+      .attach('timetable_file', fs.readFileSync(sampleFilePath), 'timetable_file.csv')
       .end((err, res) => {
+        console.log(res.body);
         if (err) {
           console.log(err);
         }
-        expect(res.body.code)
+        expect(res)
           .to
           .have
-          .equal(200);
-        expect(res.body)
-          .to
-          .have
-          .property('message');
-        expect(res.body.data.type)
-          .to
-          .equal('Isosceles');
+          .status(200);
         done();
       });
 
